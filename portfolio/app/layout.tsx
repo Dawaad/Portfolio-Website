1d1e686725eba7975f08bcfda1d45a6b056322b9
Dawaad <jntucker02@gmail.com>
2023-03-13 21:11:37 +1100

    Updated layout

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -4,8 +4,6 @@ import "../styles/globals.css";
 // import Footer from "./Footer";
 import Navbar from "./navbar";
 
-
-
 export default function RootLayout({
   children,
 }: {
@@ -13,16 +11,15 @@ export default function RootLayout({
 }) {
   return (
     <>
-    <html className="dark:bg-zinc-900 text-zinc-200" lang="en">
-      <body>
-      
-        <Navbar />
-        
-        {children}
-    
-        <footer>{/* <Footer /> */}</footer>
-      </body>
-    </html>
+      <html className="bg-zinc-900" lang="en">
+        <body>
+          <Navbar />
+
+          {children}
+
+          {/* <footer><Footer /></footer> */}
+        </body>
+      </html>
     </>
   );
 }
