8a238991a890995c44a7090ececa79b449179e6e
Dawaad <jntucker02@gmail.com>
2023-03-16 19:57:57 +1100

    Update layout.tsx

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -4,9 +4,9 @@ import "../styles/globals.css";
 // import Footer from "./Footer";
 import Navbar from "./navbar";
 
-import {Rubik} from 'next/font/google'
+import { Rubik } from "next/font/google";
 
-const rubik = Rubik({subsets:['latin'], variable: '--font-rubik'})
+const rubik = Rubik({ subsets: ["latin"], variable: "--font-rubik" });
 
 export default function RootLayout({
   children,
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <>
       <html className={`bg-zinc-900 ${rubik.variable} font-sans`} lang="en">
-        <body>
+        <body className="overflow-x-hidden">
           <Navbar />
 
           {children}
