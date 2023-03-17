32b54eba583e0cc3ccfca415d1371ab999201ea9
Dawaad <jntucker02@gmail.com>
2023-03-17 16:40:10 +1100

    Delete unecessary routes

--- portfolio/app/projects/[projectName]/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function projectDetailLayout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
-}
-
-export default projectDetailLayout;
